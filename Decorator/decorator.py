093e002ca91c35a2ff509495b20a4152963c8296
DevWilsonF <108008266+DevWilsonF@users.noreply.github.com>
2022-11-03 21:58:21 -0500

    Create decorator.py

---
+++ Decorator/decorator.py
@@ -0,0 +1,13 @@
+def funcion_a(funcion_b):
+    def funcion_c(*args, **kwargs):
+        print('Antes de la ejecución de la función a decorar')
+        result = funcion_b(*args, **kwargs)
+        print('Después de la ejecución de la función a decorar')    
+
+        return result
+
+    return funcion_c
+
+@funcion_a
+def suma(a, b):
+    return a + b
